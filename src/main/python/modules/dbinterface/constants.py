558835230341adcb938d78106b9c3c1c76431c31
Simon Hackl <si.ha@live.de>
2019-02-15 22:59:07 +0100

    - Added constants for the dbinterface module.

---
+++ src/main/python/modules/dbinterface/constants.py
@@ -0,0 +1,34 @@
+"""
+Script to store constants used by classes and functions of the dbinterface module.
+"""
+
+PYTHON_EXEC = 'python '
+REDIS_BULK_DIRECTORY = 'parser/redis_bulk.py'
+WORK_DIRECTORY = 'wrk/'
+
+DATA_SEPARATOR = '__'
+MERGE_SEPARATOR = '___'
+DISPLAY_SEPARATOR = ','
+ID = 'id'
+
+CREATE = ' create '
+VERTEX = 'vertex'
+SOURCE = 'source'
+TARGET = 'target'
+EDGE = 'edge'
+MATCH = ' match '
+DISTINCT = ' return distinct '
+LIMIT = ' limit '
+ONE = '1'
+
+G_QUERY = 'GRAPH.QUERY'
+G_DELETE = 'GRAPH.DELETE'
+G_EXPLAIN = 'GRAPH.EXPLAIN'
+QUIT = 'QUIT'
+SAVE = 'SAVE'
+KEYS = 'KEYS *'
+UTF_8 = 'UTF-8'
+
+KEY = 'key'
+NODE = 'node'
+DATA = 'data'

96869e1019f6a7156c76741fdfea0548b97a02d7
Simon Hackl <si.ha@live.de>
2019-02-15 22:59:45 +0100

    - Deleted deprecated cypher construction functions.

--- src/main/python/modules/dbinterface/cypher.py
+++ src/main/python/modules/dbinterface/cypher.py
@@ -1,122 +1,66 @@
-from modules.old.Tags import CREATE, VERTEX, SOURCE, TARGET, ID, EDGE, MATCH, WHERE, IS, \
-    DISTINCT, OR, DELETE, SET, DATA_SEPARATOR, RETURN
-
-
-def _dict_to_string(dictionary):
-
-        string = """{"""
-        last = len(dictionary) - 1
-
-        for index, key in enumerate(dictionary):
-
-            if index == last:
-                string = string + key + """:""" + """'""" + dictionary[key] + """'"""
-            else:
-                string = string + key + """:""" + """'""" + dictionary[key] + """'""" + """, """
-
-        string = string + """}"""
-
-        return string
-
-
-def _wrap(string):
-    return """'""" + string + """'"""
-
-
-def create_vertex(*args, label_=VERTEX):
-
-    return ''.join(
-        map(
-            lambda attributes: ' ,(' + attributes[ID] + ':' + label_ + ' ' + _dict_to_string(attributes) + ')'
-            if args.index(attributes) > 0
-            else '(' + attributes[ID] + ':' + label_ + ' ' + _dict_to_string(attributes) + ')',
-            args
-        )
-    )
-
-
-def delete_vertex(*args):
-
-    return MATCH + '(v)' + WHERE + '(' + ''.join(
-        map(
-            lambda identifier: OR + 'v.id' + IS + _wrap(identifier)
-            if args.index(identifier) > 0
-            else 'v.id' + IS + _wrap(identifier),
-            args
-        )
-    ) + ')' + DELETE + 'v'
+from modules.dbinterface.constants import MATCH, DISTINCT, VERTEX, LIMIT, ONE, EDGE, TARGET, SOURCE
 
 
 def get_vertex_limited():
+    """
+    Used to query one vertex.
 
-    return MATCH + '(v)' + DISTINCT + 'v LIMIT 1'
 
+    This is used to query only one vertex to extract possible properties
+    of a graph.
 
-def create_edge(*args, type_=EDGE):
+    Returns
+    -------
+    str
+        The constructed OpenCypher query to retrieve one vertex.
+    """
 
-    return ''.join(
-        map(
-            lambda attributes: ', (' + attributes[SOURCE] + ')-'
-                               + '[:' + type_ + ' ' + _dict_to_string(attributes) + ']'
-                               + '->(' + attributes[TARGET] + ')',
-            args
-        )
-    )
+    return MATCH + '(' + VERTEX + ')' + DISTINCT + VERTEX + LIMIT + ONE
 
 
 def get_edge_limited():
-
-    return MATCH + '()-[edge]->()' + DISTINCT + 'edge LIMIT 1'
-
-
-def get_merge_entities():
-
-    return \
-        MATCH + '(v)-[e]->(w)' + \
-        DISTINCT + 'v, w, e'
+    """
+    Used to query one edge.
 
 
-def get_merge_values(*args):
+    This is used to query only one edge to extract possible properties
+    of a graph.
 
-    return MATCH + '(v)' + RETURN + ', '.join(['v.' + arg for arg in args])
+    Returns
+    -------
+    str
+        The constructed OpenCypher query to retrieve one edge.
+    """
+
+    return MATCH + '()-[' + EDGE + ']->()' + DISTINCT + EDGE + LIMIT + ONE
 
 
-def graph_to_cypher(graph, label_=VERTEX, type_=EDGE):
+def get_entities():
+    """
+    Used to query all entities of a graph.
 
-    return CREATE + create_vertex(*graph['vertices'], label_=label_) + create_edge(*graph['edges'], type_=type_)
+    Returns
+    -------
+    str
+        The constructed OpenCypher query to retrieve all (connected) entities.
+    """
+
+    return MATCH + '(' + SOURCE + ')-[' + EDGE + ']->(' + TARGET + ')' + DISTINCT + ', '.join([SOURCE, TARGET, EDGE])
 
 
-def annotation_dict_to_cypher(target_property, map_property, property_prefix, dictionaries):
+def get_values(*args: str) -> str:
+    """
+    Used to retrieve all possible values of the properties given in args.
 
-    # TODO: If not all Vertices are matched, no property is set... Try atomic queries instead.
+    Parameters
+    ----------
+    args : str
+        Arguments should be only strings.
 
-    match = MATCH
-    set = SET
-    dictionary_index = 0
+    Returns
+    -------
+    str
+        The constructed OpenCypher query to retrieve all values of the specified properties.
+    """
 
-    for dictionary in dictionaries:
-
-        if dictionary_index > 0:
-
-            match += ','
-            set += ','
-
-        match += ' (v' + str(dictionary_index) + ' {' + target_property + ': ' + _wrap(dictionary.pop(map_property)) + '}' ') '
-
-        property_index = 0
-
-        for key, value in dictionary.items():
-
-            if property_index > 0:
-
-                set += ','
-
-            set += ' v' + str(dictionary_index) + '.' + property_prefix + DATA_SEPARATOR + key + IS + _wrap(value) + ' '
-
-            property_index += 1
-
-        dictionary_index += 1
-
-    print(match + set)
-
-    return match + set
+    return MATCH + '(v)' + DISTINCT + ', '.join(['v.' + arg for arg in args])

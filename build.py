6713a24e144503702c66cc851e75149e82949265
Simon Hackl <si.ha@live.de>
2019-02-15 23:07:16 +0100

    - Added LICENSE.txt - Added description and project dependencies to build.py

--- build.py
+++ build.py
@@ -3,15 +3,27 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
-use_plugin("copy_resources")
 use_plugin("pypi:pybuilder_smart_copy_resources")
 
-name = " BioNGraph"
+name = " BioNGraph "
+version = "dev-0.1"
+author = " Simon Tim Hackl "
+license = " Apache License, Version 2.0 modified with Commons Clause Restriction "
+url = " https://github.com/s-t-h/BioNGraph "
 default_task = "publish"
 
 
 @init
+def initialize(project):
+    project.build_depends_on('tkinter')
+    project.build_depends_on('redis')
+    project.build_depends_on('igraph')
+
+
+@init
 def set_properties(project):
+
+
 
     project.set_property('dir_dist_scripts', '')
     project.set_property('dir_dist', 'target/dist/BioNGraph')
@@ -20,29 +32,8 @@ def set_properties(project):
 
     project.set_property("smart_copy_resources_basedir", "src/main/resources")
     project.set_property("smart_copy_resources", {
-        "UserGuide.html": "target/dist/BioNGraph/resources",
+        "sample.zip": "target/dist/BioNGraph/resources",
+        "user_manual.pdf": "target/dist/BioNGraph/resources",
         "icons.zip": "target/dist/BioNGraph/resources",
         "icon.ico": "target/dist/BioNGraph/resources"
     })
-
-'''
-@task("redis", description="Downloads and sets up Redis-Server/Redis-Graph-Module")
-def set_up_redis():
-
-    request.urlretrieve("http://download.redis.io/releases/redis-stable.tar.gz", filename="redis.tar.gz")
-    tar = open("redis.tar.gz")
-    tar.extractall()
-    tar.close()
-
-    request.urlretrieve("https://github.com/RedisLabsModules/RedisGraph.git", filename="redis-graph.zip")
-    zip = ZipFile('redis-graph.zip', 'r')
-    zip.extractall('redis-graph')
-    zip.close()
-
-    remove("redis.tar.gz")
-    remove("redis-graph.zip")
-    #tar = open("redis.tar.gz")
-    #tar.extractall()
-    #tar.close()
-    #remove("redis.tar.gz")
-'''
